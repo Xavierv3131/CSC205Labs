db2a27e9a1e425ea279c54dc72c83cca46d5d6bf
ryanv8941 <38712985+ryanv8941@users.noreply.github.com>
2019-02-05 18:34:24 -0500

    Update TextAnalyzer.java

--- LAB02/TextAnalyzer.java
+++ LAB02/TextAnalyzer.java
@@ -1,5 +1,45 @@
+
 package LAB02;
 
+import java.util.*;
+import java.io.*;
+
+
+/**
+ * 
+ * 
+ * @author rvale2
+ *
+ */
 public class TextAnalyzer {
 
+	private int lineCount;
+	private int wordCount;
+	private int[] frequencies = new int[26];
+	
+	
+	public void analyzeText(String fileName) {
+		
+		Scanner fileIn = null;
+		try {
+			fileIn = new Scanner (new File(fileName));
+		}
+		catch(FileNotFoundException e){
+			System.out.println("File: " + fileName + " is not found");
+		}
+		
+		int count = 0;
+		while (fileIn.hasNextLine()) {
+			count ++;
+			fileIn.hasNextLine();
+		}
+		lineCount = count;
+		
+		
+		
+	}
+	
+	public int getLineCount() {return lineCount;}
+	public int getWordCount() {return wordCount;}
+	public int[] getFrequencies() {return frequencies;}
 }
